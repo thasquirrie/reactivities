506452185dc8b02a1385434e298114703b77ce5b
Eredun <thasquirrie@gmail.com>
2023-12-22 02:32:45 +0100

    Added a new client-app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,36 +2,37 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Button, Header, HeaderContent, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then((response) => {
-      setActivities(response.data);
-    });
+    const fetchData = async () => {
+      const { data } = await axios({
+        method: 'GET',
+        url: 'http://localhost:5000/api/activities',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+
+      setActivities(data);
+    };
+
+    fetchData();
   }, []);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <ul>
-          {activities.map((activity: any) => (
-            <li key={activity.id}>{activity.title}</li>
-          ))}
-        </ul>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h2' icon={'users'} content='Reactivities' />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+      <Button content='Text' />
     </div>
   );
 }
