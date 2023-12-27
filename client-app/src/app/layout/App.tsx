69a79d3c5bc72d1715287011beb340d7295f2797
Eredun <thasquirrie@gmail.com>
2023-12-27 09:20:17 +0100

    Typescript interface for Activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
-import { Button, Header, HeaderContent, List } from 'semantic-ui-react';
+import { Button, Header, List } from 'semantic-ui-react';
+import { Activity } from './models/activity';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios({
+      const { data } = await axios<Activity[]>({
         method: 'GET',
         url: 'http://localhost:5000/api/activities',
         headers: {
@@ -28,7 +27,7 @@ function App() {
     <div>
       <Header as='h2' icon={'users'} content='Reactivities' />
       <List>
-        {activities.map((activity: any) => (
+        {activities.map((activity) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
         ))}
       </List>
