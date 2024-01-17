082112ac0e3bdd2991f3db9f05998d12a368e40a
Eredun <thasquirrie@gmail.com>
2024-01-17 12:57:50 +0100

    Handling Exceptions

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Core;
 using MediatR;
 using Persistence;
 
@@ -9,29 +6,81 @@ namespace Application.Activities
 {
     public class Delete
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
+
             public Handler(DataContext context)
             {
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                Console.WriteLine(request);
                 var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null) return null;
 
                 _context.Remove(activity);
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync() > 0;
 
-                return Unit.Value;
+                if (!result) return Result<Unit>.Failure("Failed to delete the activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
 }
+
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Application.Core;
+// using MediatR;
+// using Persistence;
+
+// namespace Application.Activities
+// {
+//     public class Delete
+//     {
+//         public class Command : IRequest<Result<Unit>>
+//         {
+//             public Guid Id { get; set; }
+//         }
+
+//         public class Handler : IRequestHandler<Command, Result<Unit>>
+//         {
+//             private readonly DataContext _context;
+//             public Handler(DataContext context)
+//             {
+//                 _context = context;
+//             }
+
+//             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+
+//             {
+//                 Console.WriteLine(request);
+//                 var activity = await _context.Activities.FindAsync(request.Id);
+
+//                 if (activity == null) return null;
+
+//                 _context.Remove(activity);
+
+//                 var result = await _context.SaveChangesAsync() > 0;
+
+//                 if (!result) return Result<Unit>.Failure("Failed to delete activity");
+
+//                 return Result<Unit>.Success(Unit.Value);
+//             }
+//         }
+//     }
+// }
