5c1c0f3da4d852bbdb7d7e0e64076500e1280493
Eredun <thasquirrie@gmail.com>
2024-01-18 16:20:10 +0100

    Redirected "bad guid" error to not-found error page

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -21,9 +21,12 @@ axios.interceptors.response.use(
     return response;
   },
   (error: AxiosError) => {
-    const { data, status } = error.response as AxiosResponse;
+    const { data, status, config } = error.response as AxiosResponse;
     switch (status) {
       case 400:
+        if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
+          router.navigate('/not-found');
+        }
         if (data.errors) {
           const modalStateErrors = [];
           for (const key in data.errors) {
